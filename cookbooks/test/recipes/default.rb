54e9f947e11238f597cdfd4e59e0e15fd7febb74
bipinbachhao <bipinbachhao@gmail.com>
2016-09-27 19:46:23 -0400

    Cleaned repo

--- cookbooks/test/recipes/default.rb
+++ cookbooks/test/recipes/default.rb
@@ -2,10 +2,24 @@
 # Cookbook Name:: test
 # Recipe:: default
 #
-# Copyright 2016, YOUR_COMPANY_NAME
+# Copyright 2016, Bipin Bachhao
 #
 # All rights reserved - Do Not Redistribute
 #
 package "git"
 
 log "Well, that was too easy"
+
+group 'bipin_group' do
+  gid '5000'
+  non_unique true
+  action :create
+end
+
+user 'bipin' do
+  uid '5000'
+  gid '5000'
+  home '/home/bipin'
+  shell '/bin/bash'
+  action :create
+end
