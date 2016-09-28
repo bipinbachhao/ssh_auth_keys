9cef7359bf05479e99dddb86c7c86a4b232741d0
bipinbachhao <bipinbachhao@gmail.com>
2016-09-27 20:16:09 -0400

    removed cookbook folder

--- cookbooks/test/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'test'
-maintainer       'Bipin Bachhao'
-maintainer_email 'bipinbachhao@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures test'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends 'ssh_auth_keys'
